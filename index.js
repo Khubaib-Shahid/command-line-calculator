27db2f2c2eab364539e5bcf89f28d2611cd1b8e8
Khubaib Shahid <139511144+Khubaib-Shahid@users.noreply.github.com>
2024-03-20 02:36:37 +0500

    command line calculator completed

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let answers = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "num1" },
+    { message: "Enter second number", type: "number", name: "num2" },
+    { message: "Choose operator to perform action", type: "list", name: "operator", choices: ["Addition", "Substraction", "Multiplication", "Division"] }
+]);
+if (answers.operator === "Addition") {
+    console.log(answers.num1 + answers.num2);
+}
+else if (answers.operator === "Substraction") {
+    console.log(answers.num1 - answers.num2);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.num1 * answers.num2);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.num1 / answers.num2);
+}
